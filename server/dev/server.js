7ad32d1b80e366e61ae96b95cea82799ccf83e0d
Ranacode <romerodeveloper@techie.com>
2018-01-27 17:29:39 +0000

    setup socketserver and better error handler in server.listen

--- server/dev/server.js
+++ server/dev/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import SocketServer from 'socket.io';
 import app from './config/express';
 import mongoDB from './config/database';
 
@@ -6,11 +8,13 @@ to allow server start listening*/
 mongoDB
 	.then(() => {
 		console.log('CONNECTED TO MONGODB SUCCESFULLY');
-		const PORT = process.env.PORT || 8000;
+		//Socket server setup
+		const server = http.createServer(app);
+		const io = new SocketServer(server);
 
-		app.listen(PORT, (error) => {
-			if (error) return next(error);
-			console.log(`SERVER LISTENING ON PORT ${PORT}...`);
+		const PORT = process.env.PORT || 8000;
+		server.listen(PORT, (error) => {
+			error ? process.exit() : console.log(`SERVER LISTENING ON PORT ${PORT}...`);
 		});
 	})
 	.catch((error) => console.error(error));
